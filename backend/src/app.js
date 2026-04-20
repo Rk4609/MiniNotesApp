1ca3fe813eeb939a1f40bb4548082bb89d675640
Rk4609 <rakeshjangid8742@gmail.com>
2026-04-20 13:12:04 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,23 @@
-import express from "express"
-import cors from "cors"
-import cookieparser from "cookie-parser"
-const app = express()
+import express from "express";
+import cors from "cors";
+import cookieparser from "cookie-parser";
+const app = express();
 
 app.use(
   cors({
-    origin:["http://localhost:5173",
-      "https://mini-notes-app-three.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://mini-notes-app-three.vercel.app",
+    ],
     credentials: true,
-     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
-)
+);
 app.options("*", cors());
+app.use(express.json());
+app.use(cookieparser());
 
-app.use(cookieparser())
-app.use(express.json())
-
-//routes 
-import noteRouter from "./routes/note.routes.js"
-app.use("/api/notes",noteRouter)
-export { app }
+//routes
+import noteRouter from "./routes/note.routes.js";
+app.use("/api/notes", noteRouter);
+export { app };
