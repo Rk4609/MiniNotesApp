db6ce0686d419d4c8c5817344b4f19e9a7036c83
Rk4609 <rakeshjangid8742@gmail.com>
2026-04-20 14:24:17 +0530

    url error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,23 @@
-import express from "express";
-import cors from "cors";
-import cookieparser from "cookie-parser";
-const app = express();
+import express from "express"
+import cors from "cors"
+import cookieparser from "cookie-parser"
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://mini-notes-app-three.vercel.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-     allowedHeaders: ["Content-Type", "Authorization"]
-  }),
-);
-app.use(express.json());
-app.use(cookieparser());
+const app = express()
 
-//routes
-import noteRouter from "./routes/note.routes.js";
-app.use("/api/notes", noteRouter);
-export { app };
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://mini-notes-app-azure.vercel.app/"
+  ],
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
+app.use(express.json())
+app.use(cookieparser())
+
+import noteRouter from "./routes/note.routes.js"
+app.use("/api/notes", noteRouter)
+
+export { app }
