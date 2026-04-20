4677becd46dae2f206ba34ba66cdf230d156663b
Rk4609 <rakeshjangid8742@gmail.com>
2026-04-20 13:17:46 +0530

    header

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+     allowedHeaders: ["Content-Type", "Authorization"]
   }),
 );
 app.options("*", cors());
