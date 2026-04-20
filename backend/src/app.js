298b70efd9af6f4b69e23de9934d293a748247a4
Rk4609 <rakeshjangid8742@gmail.com>
2026-04-20 14:28:18 +0530

    error

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,20 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin)
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200)
+  }
+
+  next()
+})
+
+
 app.use(express.json())
 app.use(cookieparser())
 
