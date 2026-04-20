b9e4217fd88c164aaa5ebdcf0b308bd0ce8971cd
Rk4609 <rakeshjangid8742@gmail.com>
2026-04-20 12:54:36 +0530

    slash

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(
   cors({
-    origin:["http://localhost:5173/","https://mini-notes-app-three.vercel.app/"],
+    origin:["http://localhost:5173","https://mini-notes-app-three.vercel.app"],
     credentials: true,
   }),
 )
