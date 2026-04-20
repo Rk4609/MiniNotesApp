3396afa348e67fb15d6cfc690146f3e8e9a3627b
Rk4609 <rakeshjangid8742@gmail.com>
2026-04-20 14:32:16 +0530

    change

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,26 +6,12 @@ const app = express()
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
     "https://mini-notes-app-azure.vercel.app/"
   ],
   credentials: true,
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin)
-  res.header("Access-Control-Allow-Credentials", "true")
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200)
-  }
-
-  next()
-})
 
 
 app.use(express.json())
